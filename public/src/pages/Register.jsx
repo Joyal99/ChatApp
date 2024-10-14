b2e9d54a7c50c7a32a4edb00f23bf87e17afc9cc
Joyal Biju K. <122915447+Joyal99@users.noreply.github.com>
2024-10-14 15:53:03 -0400

    Backend working for register page with mongodb integration

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,15 +1,82 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Logo from '../assets/logo.svg';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from 'axios';
+import { registerRoute } from '../utils/APIRoutes';
 
 function Register() {
-    const handleSubmit = (event) => {
-        event.prventDefault();
-        alert("form");
+    const navigate = useNavigate();
+    const [values,setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+    const toastOptions= {
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if(handleValidation()) {
+          const { password, confirmPassword, username, email } = values; 
+            const {data} = await axios.post(registerRoute, {
+                username,
+                email,
+                password,
+            });
+            if(data.status === false) {
+                toast.error(data.msg, toastOptions);
+            }
+            if(data.status === true) {
+                localStorage.setItem('chat-app-user', JSON.stringify(data.user));
+                navigate("/");
+            }
+        }
     };
 
-    const handleChange = (event) => {}
+    const handleValidation = () => {
+        const { password, confirmPassword, username, email } = values;
+        if(password !== confirmPassword) {
+            toast.error(
+                "Password and Confirm Password do not match!", 
+                toastOptions
+            );
+            return false;
+        }else if (username.length<3) {
+            toast.error(
+                "Username must be at least 3 characters!", 
+                toastOptions
+            ); 
+            return false;
+        }else if (password.length < 8) {
+            toast.error(
+                "Password must be equal to or greater than 8 characters!", 
+                toastOptions
+            );
+            return false;
+        }else if(email==="") {
+            toast.error(
+                "Email is required!", 
+                toastOptions
+            );
+            return false;
+        }
+        return true;
+    };
+    
+    const handleChange = (event) => {
+        setValues({ ...values, [event.target.name]: event.target.value });
+    };
+
   return( 
   <>
     <FormContainer>
@@ -47,8 +114,9 @@ function Register() {
                 Already have an account? <Link to="/login">Login</Link>
             </span>
         </form>
-    </FormContainer>;
-  </>
+     </FormContainer>
+     <ToastContainer />
+    </>
   );
 }
 
