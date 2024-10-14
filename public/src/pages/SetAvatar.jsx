499d34c54239bde10a68d0cad20db52853a57754
Joyal Biju K. <122915447+Joyal99@users.noreply.github.com>
2024-10-14 16:53:19 -0400

    Setting SetAvatar Route

---
+++ public/src/pages/SetAvatar.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import loader from '../assets/loader.gif';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from 'axios';
+import { setAvatarRoute } from '../utils/APIRoutes';
+
+export default function SetAvatar() {
+  return (
+    <div>SetAvatar</div>
+  )
+}
