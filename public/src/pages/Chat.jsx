df8713a0343c08b4bdd4c65db252cfde75f07ff9
Joyal Biju K. <122915447+Joyal99@users.noreply.github.com>
2024-10-17 16:40:05 -0400

    Set up Chat Container

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 function Chat() {
+    const navigate = useNavigate();
+    const [contacts, setContacts] = useState([]);
+    const [currentUser, setCurrentUser] = useState(undefined);
+    useEffect(async() => {
+        if (!localStorage.getItem('chat-app-user')) {
+            navigate("/login");
+          } else {
+            setCurrentUser(await JSON.parse(localStorage.getItem('chat-app-user')));
+          }
+    }, []);
+    useEffect(async() => {
+        if(currentUser) {
+            if(currentUser.isAvatarImageSet) {
+                const data = await axios.get(`$*(allUsersRoute)/${currentUser._id}`);
+                setContacts(data.data);
+            }else {
+                navigate("/setAvatar");
+            }
+        }
+    }, [currentUser]);
     return (
         <Container>
             <div className="container">
-
+                <Contacts contacts={contacts} />
             </div>
         </Container>
     );
