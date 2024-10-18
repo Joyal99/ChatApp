1bb275a8a8dbcb88ad0d119dba04dabd9a78b0aa
Joyal Biju K. <122915447+Joyal99@users.noreply.github.com>
2024-10-18 16:33:54 -0400

    Configured socket.io for server backend of the chatapp

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const userRoutes = require("./routes/userRoutes");
 const messageRoute = require("./routes/messagesRoute");
 const app = express();
+const socket = require("socket.io");
 require("dotenv").config();
 
 app.use(cors());
@@ -26,4 +27,27 @@ mongoose
 
 const server = app.listen(process.env.PORT,()=> {
     console.log(`Server is running on port: ${process.env.PORT}`);
+});
+
+const io = socket(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        credentials: true,
+    },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+        onlineUsers.set(userId, socket.id);
+    });
+
+    socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if(sendUserSocket){
+            socket.to(sendUserSocket).emit("receive-msg", data.msg);
+        }
+    });
 });
