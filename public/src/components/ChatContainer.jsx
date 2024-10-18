1a6b2fad2aa3d1c2e3d2c2b55a5f0b1e1a4eea14
Joyal Biju K. <122915447+Joyal99@users.noreply.github.com>
2024-10-17 20:27:48 -0400

    Added chat input packages

--- public/src/components/ChatContainer.jsx
+++ public/src/components/ChatContainer.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import Logout from './Logout';
+import ChatInput from './ChatInput';
 
 export default function ChatContainer({currentChat}) {
+    const handleSendMsg = async(msg) => {};
   return (
     <>
     {currentChat && (
@@ -22,7 +24,7 @@ export default function ChatContainer({currentChat}) {
             <Logout />
         </div>  
         <div className="chat-messages"></div>
-        <div className="chat-input"></div>  
+        <ChatInput handleSendMsg={handleSendMsg} /> 
     </Container>
     )}
    </>
