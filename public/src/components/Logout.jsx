21f828a1254ccf71df0edb9637bfe12a384e24ad
Joyal Biju K. <122915447+Joyal99@users.noreply.github.com>
2024-10-17 20:15:13 -0400

    Logout button in chat header works, and redirects to /login page

--- public/src/components/Logout.jsx
+++ public/src/components/Logout.jsx
@@ -9,7 +9,25 @@ export default function Logout() {
   const handleClick = async () => {
     localStorage.clear();
     navigate("/login");
-  }
-    return <div>Logout</div>
+  };
+  return ( 
+    <Button onClick={handleClick}>
+        <BiPowerOff />
+    </Button>
+);
 }
 
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  background-color: #9a86f3;
+  border: none;
+  cursor: pointer;
+  svg {
+    font-size: 1.3rem;
+    color: #ebe7ff;
+  }
+`;
