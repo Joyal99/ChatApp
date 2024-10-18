1e2396d950fb8e4f2c90af867fc6cf7715625132
Joyal Biju K. <122915447+Joyal99@users.noreply.github.com>
2024-10-17 20:02:08 -0400

    configured chat container with userlogo in the chat header

---
+++ public/src/components/Logout.jsx
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import {BiPowerOff} from 'react-icons/bi';
+
+export default function Logout() {
+  const navigate = useNavigate();
+  const handleClick = async () => {
+    localStorage.clear();
+    navigate("/login");
+  }
+    return <div>Logout</div>
+}
+
